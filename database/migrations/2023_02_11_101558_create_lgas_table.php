45ddb542d3b6a87effe305925682e3a3d8693140
chrisdavid32 <chrisdavid3271@gmail.com>
2023-02-11 11:32:36 +0100

    add table model and migration

--- database/migrations/2023_02_11_101558_create_lgas_table.php
+++ database/migrations/2023_02_11_101558_create_lgas_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lgas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('state_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+
+            $table->foreign('state_id')->references('id')->on('lgas')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lgas');
     }
 };
